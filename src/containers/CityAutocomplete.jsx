020d7ccaf52d79bd1f81f810d9ac5057bc879256
User <vekaev4@gmail.com>
2020-08-11 08:26:10 +0300

    change request sending

--- src/containers/CityAutocomplete.jsx
+++ src/containers/CityAutocomplete.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import axios from 'axios';
 import { Select } from 'antd';
 import { cityList } from '../cityList';
-import { Redirect, useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 const { Option } = Select;
 
 export const CityAutocomplete = () => {
   const history = useHistory();
+  let { topicId } = useParams();
 
-  let sendRequest = (value) => {
+  console.log(history);
+  let sendRequest = (status, value) => {
     console.log(value);
     history.push(`/${value}`);
     axios
       .get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=e70b3d12e10d1735e93d7770e126a258`,
+        status == 'current'
+          ? `http://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&appid=e70b3d12e10d1735e93d7770e126a258`
+          : `http://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&appid=e70b3d12e10d1735e93d7770e126a258`,
       )
       .then((res) => {
         console.log(res);
@@ -29,7 +33,8 @@ export const CityAutocomplete = () => {
       style={{ width: 400 }}
       placeholder='Select capital'
       optionFilterProp='children'
-      onChange={sendRequest}
+      defaultValue={topicId ? topicId : ''}
+      onChange={() => sendRequest('choosen_city', value)}
       filterOption={(input, option) =>
         option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
       }
